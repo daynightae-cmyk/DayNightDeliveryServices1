574cb2b220e39ef1abcd1bc5d53b2a8a6d7b5def
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-03 06:56:28 +0400

    Load support polish stylesheet

--- artifacts/day-night-delivery/src/main.tsx
+++ artifacts/day-night-delivery/src/main.tsx
@@ -6,6 +6,7 @@ import { reportError } from './lib/monitoring';
 import './index.css';
 import './styles/dn-premium.css';
 import './styles/dn-ui-fixes.css';
+import './styles/dn-support-polish.css';
 
 const FALLBACK_LOGO = 'https://i.postimg.cc/BnMJh77T/Chat-GPT-Image-Jun-23-2026-05-21-26-PM.png';
 
