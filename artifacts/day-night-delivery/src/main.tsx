f6b2f6fb1c3ef7b6b1b3d18a3ee5f00636ed15f7
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-22 03:48:27 +0400

    fix(ui): load final merchant and admin production rescue last

--- artifacts/day-night-delivery/src/main.tsx
+++ artifacts/day-night-delivery/src/main.tsx
@@ -22,6 +22,7 @@ import "./styles/dn-portal-figma-reference-v6.css";
 import "./styles/dn-portal-figma-final-v7.css";
 import "./styles/dn-portal-notification-final.css";
 import "./styles/dn-operations-control-rescue.css";
+import "./styles/dn-production-visual-rescue-v3.css";
 
 const FALLBACK_LOGO = "https://i.postimg.cc/BnMJh77T/Chat-GPT-Image-Jun-23-2026-05-21-26-PM.png";
 
